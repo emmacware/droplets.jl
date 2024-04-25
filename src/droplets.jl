047a2f67aeeec022db780bf9ef2516ae5d2a2094
emmacware <ecware@ucdavis.edu>
2024-04-25 11:50:13 -0700

    deleted pesky case insensitive file

--- src/droplets.jl
+++
@@ -1,9 +0,0 @@
-module Droplets
-# under construction!
-# add functions from SDfunc folder and export them
-include("SDfunc/coalescence.jl")
-include("SDfunc/condensation.jl")
-
-export init_ξ_const
-
-end
