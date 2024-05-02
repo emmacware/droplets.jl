12438e65dbc2f311919486e9ba012c669abd180c
emmacware <ecware@ucdavis.edu>
2024-05-02 09:08:11 -0700

    include files

--- src/Droplets.jl
+++ src/Droplets.jl
@@ -3,6 +3,11 @@ module Droplets
 # add functions from SDfunc folder and export them
 include("SDfunc/coalescence.jl")
 include("SDfunc/condensation.jl")
+include("SDfunc/constants.jl")
+include("SDfunc/DSDvis.jl")
+include("SDfunc/setup.jl")
+include("SDfunc/updateposition.jl")
+
 
 export init_ξ_const
 
