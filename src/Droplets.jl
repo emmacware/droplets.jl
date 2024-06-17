c07903b445e94a262ecdf70f36aa164de3ffd654
obin1 <psturm@usc.edu>
2024-06-17 09:54:54 -0400

    add module level constants (g, Rd, Cp)

--- src/Droplets.jl
+++ src/Droplets.jl
@@ -1,12 +1,14 @@
 module Droplets
-# under construction!
-include(joinpath("SDfunc", "constants.jl"))
-include(joinpath("SDfunc", "coalescence.jl"))
-include(joinpath("SDfunc", "condensation.jl"))
-include(joinpath("SDfunc", "setup.jl"))
-include(joinpath("SDfunc", "updateposition.jl"))
+    # under construction!
+    include(joinpath("SDfunc", "constants.jl"))
+    include(joinpath("SDfunc", "coalescence.jl"))
+    include(joinpath("SDfunc", "condensation.jl"))
+    include(joinpath("SDfunc", "setup.jl"))
+    include(joinpath("SDfunc", "updateposition.jl"))
 
-
-#export init_ξ_const
+    gconst = 9.81  # gravitational constant
+    Rd = 287       # gas constant for dry air
+    Cp = 1005      # specific heat capacity of dry air
+    #export init_ξ_const
 
 end
