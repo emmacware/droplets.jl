9260997c7b8e977f39f701779fad0fa7f6baf4e2
emmacware <ecware@ucdavis.edu>
2024-06-19 19:54:14 -0700

    example setup

--- src/SDfunc/supercrystals.jl
+++ src/SDfunc/supercrystals.jl
@@ -28,3 +28,68 @@ function create_crystals(Ns,Nx,Ny,Δx,Δy,astart,cstart,ξstart,Mstart,ρistart,
     end
     return crystals 
 end
+
+################################################################################
+# Imagine a setup like this!!:
+
+Base.@kwdef struct deposition_params{FT} <: {FT} #not sure how to do the type here
+    deposition = "Capacitance" # "None", "Capacitance", "DepositionCoeff"
+    ρ_dep_p = "Constant" # "Constant", "Optimized"
+    constants = constants()
+    const_dt = 1
+    solver = "Euler" # Can we do this? Not sure how to pass
+    #etc...
+end
+
+function run_deposition!(deposition_params,crystals,statevar...)
+    if deposition_params.deposition == "Capacitance"
+        #do capacitance deposition
+        de_params = capacitance(constants,..)
+    elseif deposition_params.deposition == "DepositionCoeff"
+        #do deposition coeff
+        de_params = deposition_coeff(constants,..)
+    elseif deposition_params.deposition == "None"
+        de_params = empty()
+    end
+
+    if deposition_params.ρ_dep_p == "Constant"
+        ρdep_params = constant_ρdep(de_params,..)
+    elseif deposition_params.ρ_dep_p == "Optimized"
+        ρdep_params = optimized_ρdep(de_params,..)
+    end
+
+    # Parameters for the ODE solver
+    p = (
+        de_params = de_params,
+        ρdep_params = ρdep_params,
+        constants = deposition_params.constants,
+        #etc...
+    )
+
+    problem = ODE.ODEProblem(run_deposition, crystals_and_state, (t0,tend), p)
+    return ODE.solve(problem,ODE.Euler(),dt = deposition_params.const_dt)
+end
+
+
+function run_deposition(du,u,p,t)
+    de_params = p.de_params
+    ρdep_params = p.ρdep_params
+    constants = p.constants
+    
+    crystals, statevar = u #unpacking, would look different obviously
+
+    du[crystals] = deposition_tendency(de_params,ρdep_params,constants,crystals,statevar)
+    
+    dqi = sum(crystals.ξ.*crystals.M)
+
+    du[qi] = -dqi
+    du[T] = ...
+    du[P] = ...
+
+    return du
+end
+
+
+
+
+
