acf875ef0b6c0a8b4d8a4ddb8e4eafcb32b1f3fa
emmacware <ecware@ucdavis.edu>
2025-01-16 14:53:15 +0100

    resetup tests

--- test/golovintest.jl
+++ test/golovintest.jl
@@ -1,11 +1,11 @@
 using Droplets
 using Test
-# import Pkg; Pkg.add("Pkg")
+using Pkg
+Pkg.add(["Combinatorics", "Distributions", "Random", "JSON", "DelimitedFiles", "CPUTime", "Plots"])
+using Random,Combinatorics,Distributions,CPUTime
 
 @testset begin
 
-    # Pkg.add(["Combinatorics", "Distributions", "Random", "JSON", "DelimitedFiles", "CPUTime", "Plots"])
-    # using Random,Combinatorics,Distributions,CPUTime
     include("../Examples/testfunctions.jl")
     # using 
         
@@ -17,15 +17,14 @@ using Test
     setup["DT_SEC"]= 1.
         
         # Plotting parameters
-        setup["N_BINS"]= 128
-        setup["R_BINS_MIN"]= 1e-5
-        setup["R_BINS_MAX"]= 5e-3
-        setup["NUM_CONC_PER_M3"]= 2^23
-        setup["DIAM_AT_MEAN_VOL_M"]= 2*30.531e-6
-        setup["ADDITIVE_KERNEL_COEFF"]= 1500
-        setup["T_MAX_SEC"]= 3600
-        setup["PLOT_TIME_STEP"]= 120 # seconds
-    
+    setup["N_BINS"]= 128
+    setup["R_BINS_MIN"]= 1e-5
+    setup["R_BINS_MAX"]= 5e-3
+    setup["NUM_CONC_PER_M3"]= 2^23
+    setup["DIAM_AT_MEAN_VOL_M"]= 2*30.531e-6
+    setup["ADDITIVE_KERNEL_COEFF"]= 1500
+    setup["T_MAX_SEC"]= 3600
+    setup["PLOT_TIME_STEP"]= 120 # seconds
     setup["N_BIN_EDGES"] = setup["N_BINS"] + 1
 
     Ns::Int = setup["N_PART"]
@@ -51,12 +50,6 @@ using Test
     drops = runsettings_mass_density.init_method(coagsettings)
     mass_dens_bin,timing = coag_runtime(2,drops,coagsettings,runsettings_mass_density)
 
-    # dict = Dict()
-    # dict["Number Concentration (m^-3)"] = num_dens_bin
-    # dict["Mass Density (g/m^3 dlnr)"] = mass_dens_bin
-    # json_string = JSON.json(dict)
+    @test mass_dens_bin !== nothing
 
-    # open("output.json", "w") do file
-    #     write(file, json_string)
-    # end
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,31 @@
+using Droplets
 using Test
 
-@testset begin
-    include("mvp.jl")
+@testset "Droplets.jl" begin
+    FT = Float64
+    Ns = 2
+    scale = Ns * (Ns - 1) / 2 / (Ns / 2)
+    coagsettings = coag_settings{FT}(Ns=Ns,scale=scale,Δt=1)
+
+    ξ = [Int(2),Int(3)]
+    R = [FT(1.0),FT(2.0)]
+    X = 3pi/4 .*R.^3
+    I = [Int(1),Int(2)]
+    drops = droplets_allocations(ξ, R, X, I, zeros(FT, div(Ns, 2)), zeros(FT, div(Ns, 2)))
+
+    @test golovin(drops, (1,2), coagsettings) == 1500*(X[1]+X[2])
+
+    pair_Ps!(1, (1,2), drops, golovin,coagsettings)
+    @test drops.pαdt[1] == 3*1500*(X[1]+X[2])
+
+    drops.pαdt[1]=1
+    drops.ϕ[1]=0.5
+    sdm_update!((1,2),1, drops)
+
+    @test drops.ξ == [2,1]
+
 end
 
-@testset begin
+@testset "Golovin Test" begin
     include("golovintest.jl")
 end
