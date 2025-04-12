1c232c699729af564b8267b9fc272348c9cf9918
emmacware <ecware@ucdavis.edu>
2025-04-12 16:29:38 +0200

    documenter?

--- docs/make.jl
+++ docs/make.jl
@@ -4,10 +4,11 @@ makedocs(
     sitename = "Docs for Droplets.jl",
     doctest = false,
     format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
+    remotes = nothing,
     clean = false,
     pages = Any[
         "Home" => "index.md",
     ],
 )
 
-deploydocs(repo = "github.com/emmacware/droplets.jl.git", target = "build")
+deploydocs(repo = "github.com/emmacware/droplets.jl", target = "build")
